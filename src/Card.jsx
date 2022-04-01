c8e0b297150ac6adb184e173e149806ecb42af63
sam <sammybonfanti@gmail.com>
2022-04-01 02:34:56 -0700

    restyled card

--- src/Card.jsx
+++ src/Card.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 // props never change, are always just inputs we get and we never modified them
 const Card = ({ name, email, id }) => {
 	return (
-		<div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-			<img src={`https://robohash.org/${id}?200x200`} alt='robots' style={{ width: 200, height: 200 }} />
+		<div className='tc grow bg-light-green br3 pa3 ma2 dib bw2 shadow-5'>
+			<img src={`https://robohash.org/${id}?size=200x200`} alt='robots' />
 			<div>
 				<h2>{name}</h2>
 				<p>{email}</p>
