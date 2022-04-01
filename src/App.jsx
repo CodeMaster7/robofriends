bd348b9d892980f538db3f767f470378acf0ee61
sam <sammybonfanti@gmail.com>
2022-04-01 01:20:21 -0700

    import searchbox  and return it added an h1

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import CardList from './CardList'
+import SearchBox from './SearchBox'
 import { robots } from './robots'
 
 import './App.css'
@@ -7,9 +8,11 @@ import './App.css'
 class App extends Component {
 	render() {
 		return (
-			<>
+			<div className='tc'>
+				<h1>RoboFriends</h1>
+                <SearchBox />
 				<CardList robots={robots} />
-			</>
+			</div>
 		)
 	}
 }
