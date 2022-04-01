e207a8a8edf88d22000ce8026c5ea4d788fa9b7a
sam <sammybonfanti@gmail.com>
2022-04-01 01:21:30 -0700

    refactored the code

--- src/CardList.jsx
+++ src/CardList.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import Card from './Card'
 
+// pure or dumb componets, pure functions they receive something and return something
 const CardList = ({ robots }) => {
-	const cardArray = robots.map((user, i) => {
-		return <Card key={robots[i].id} id={robots[i].id} name={robots[i].name} email={robots[i].email} />
-	})
-	return <>{cardArray}</>
+	return (
+		<>
+			{
+                robots.map((user, i) => {
+				    return <Card key={user.id} id={robots[i].id} name={robots[i].name} email={robots[i].email} />
+			    })
+            }
+		</>
+	)
 }
 
 export default CardList
